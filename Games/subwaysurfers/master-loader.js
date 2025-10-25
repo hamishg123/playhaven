777c5841109ef623a00cad7126e153fa0d7d22a0
hamishg123 <hamishgordon051@gmail.com>
2025-10-25 20:01:35 +0100

    Refactor master-loader.js for improved clarity

--- Games/subwaysurfers/master-loader.js
+++ Games/subwaysurfers/master-loader.js
@@ -1,29 +1,40 @@
 "use strict";
-var scripts = document.getElementsByTagName("script"),
-    scriptUrl = scripts[scripts.length - 1].src,
-    root = scriptUrl.split("master-loader.js")[0],
-    loaders = {
-        unity: "unity.js",
-        "unity-beta": "unity-beta.js",
-        "unity-2020": "unity-2020.js"
-    };
-if (0 <= window.location.href.indexOf("pokiForceLocalLoader") && (loaders.unity = "/unity/dist/unity.js", loaders["unity-beta"] = "/unity-beta/dist/unity-beta.js", loaders["unity-2020"] = "/unity-2020/dist/unity-2020.js", root = "/loaders"), !window.config) throw Error("window.config not found");
+
+var scripts = document.getElementsByTagName("script");
+var scriptUrl = scripts[scripts.length - 1].src;
+var root = scriptUrl.split("master-loader.js")[0];
+
+// Supported loaders
+var loaders = {
+    unity: "unity.js",
+    "unity-beta": "unity-beta.js",
+    "unity-2020": "unity-2020.js"
+};
+
+// Check config
+if (!window.config) throw Error("window.config not found");
+
+// Get loader
 var loader = loaders[window.config.loader];
 if (!loader) throw Error('Loader "' + window.config.loader + '" not found');
+
+// Pick correct Unity loader based on version
 if (!window.config.unityWebglLoaderUrl) {
-    var versionSplit = window.config.unityVersion ? window.config.unityVersion.split(".") : [],
-        year = versionSplit[0],
-        minor = versionSplit[1];
+    var versionSplit = window.config.unityVersion ? window.config.unityVersion.split(".") : [];
+    var year = versionSplit[0];
+    var minor = versionSplit[1];
+
     switch (year) {
         case "2019":
-            window.config.unityWebglLoaderUrl = 1 === minor ? "UnityLoader.2019.1.js" : "UnityLoader.2019.2.js";
+            window.config.unityWebglLoaderUrl = (minor === "1") ? "UnityLoader.2019.1.js" : "UnityLoader.2019.2.js";
             break;
         default:
-            window.config.unityWebglLoaderUrl = "UnityLoader.js"
+            window.config.unityWebglLoaderUrl = "UnityLoader.js";
+            break;
     }
 }
-var sdkScript = document.createElement("script");
-sdkScript.src = "./poki-sdk.js", sdkScript.onload = function() {
-    var i = document.createElement("script");
-    i.src = root + loader, document.body.appendChild(i)
-}, document.body.appendChild(sdkScript);
+
+// ✅ Directly load Unity without SDK
+var unityScript = document.createElement("script");
+unityScript.src = root + loader;
+document.body.appendChild(unityScript);
